bff99ed768843c803c4f51a8e194ce9760326c68
Evan Zhou <evanzhoudev@gmail.com>
2026-03-14 19:55:19 -0700

    Add openai-oauth-core

---
+++ packages/openai-oauth-core/src/index.ts
@@ -0,0 +1,22 @@
+export type { AuthLoaderOptions, EffectiveAuth } from "./auth.js"
+export { deriveAccountId, loadAuthTokens, parseJwtClaims } from "./auth.js"
+export {
+	collectCompletedResponseFromSse,
+	iterateServerSentEvents,
+	type ServerSentEvent,
+} from "./sse.js"
+export {
+	CodexResponsesState,
+	type CodexResponsesStateOptions,
+	type CodexResponsesStateSnapshot,
+} from "./state.js"
+export {
+	type CodexOAuthClient,
+	type CodexOAuthSettings,
+	createCodexOAuthClient,
+	createCodexOAuthFetch,
+	DEFAULT_CODEX_BASE_URL,
+	getDefaultCodexInstructions,
+	type NormalizeCodexResponsesBodyOptions,
+	normalizeCodexResponsesBody,
+} from "./transport.js"
